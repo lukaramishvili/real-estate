4911479529c52b65bfeadfd79c837f31a4d596c3
lukaramishvili <lukaramishvili@localhost>
2012-06-25 08:23:50 +0400

    database files

---
+++ database/tables/tr.sql
@@ -0,0 +1,16 @@
+﻿-- Table: tr
+
+-- DROP TABLE tr;
+
+CREATE TABLE tr
+(
+  "ix-tr" integer NOT NULL,
+  keyword text NOT NULL,
+  lang text NOT NULL,
+  "value" text NOT NULL,
+  CONSTRAINT pri_ix_tr PRIMARY KEY ("ix-tr")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE tr OWNER TO re_user;
